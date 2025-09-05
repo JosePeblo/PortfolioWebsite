f2c41be1612eef180e7f880e7a53181ece272be9
Peblo <a01275676@tec.mx>
2025-09-04 22:29:49 -0600

    feat: enabled email form

--- src/components/index/Contact.vue
+++ src/components/index/Contact.vue
@@ -1,28 +1,55 @@
 <script setup lang="ts">
+function PostEmail() {
+  const form: HTMLFormElement | null = document.getElementById("email-form")
+  if (form === null) return;
+
+  let data = new FormData(form);
+
+  fetch(`${window.location.origin}/mail-me`, {
+    method: 'post',
+    body: data,
+  }).then(res => {
+      console.log(res.status)
+      switch (res.status) {
+        case 200: {
+          form.reset()
+        } break;
+        case 400: {
+          alert('Please send the correct fields');
+        } break;
+        case 500: {
+          alert('We had trouble processing your request try again later');
+        } break;
+      }
+    }).catch(_ => {
+      alert('We had trouble processing your request try again later');
+    })
+  return false;
+}
 </script>
 
 <template>
-  <div hidden class="email-card" id="contact">
+  <div class="email-card" id="contact">
     <h2>Send me a message</h2>
-    <form action="">
+    <form id="email-form"  @submit.prevent="PostEmail">
       <div class="form-input">
         <label for="name">Name</label>
-        <input type="text" id="name">
+        <input type="text" id="name" name="name">
       </div>
 
       <div class="form-input">
         <label for="email">Email</label>
-        <input type="text" id="email">
+        <input type="text" id="email" name="email">
       </div>
 
       <div class="form-input">
         <label for="subject">Subject</label>
-        <input type="text" id="subject">
+        <input type="text" id="subject" name="subject">
       </div>
 
       <div class="form-input">
         <label for="message">Message</label>
-        <input type="text" id="message">
+        <input type="text" id="message" name="message">
       </div>
 
       <button type="submit">
